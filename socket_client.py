475d5118c7d9e3d9e2f72d53fa59ad379ba3a1c8
Zac <97088563+zkornas@users.noreply.github.com>
2023-04-10 23:52:25 -0700

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -13,7 +13,7 @@ for i in range(3):
     s.connect((services[i], ports[i]))
     logging.debug("Connecting to service: {} on port (${})".format(services[i], ports[i]))
     if(services[i] == 'tcpbin.com'):
-        s.send("Can you hear me?".encode())
+        s.send("Can you hear me?\n".encode())
         s.settimeout(5)
     data = s.recv(1024)
     logging.debug("Receiving data")
