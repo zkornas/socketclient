ab574fb35515d87007781150e4f99d22ac3c8af4
Zac <97088563+zkornas@users.noreply.github.com>
2023-04-10 20:54:26 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('tcpbin.com', 4242))
-s.send('Hello'.encode())
-data = s.recv(1024)
-s.close()
-print("received data:", data.decode())
